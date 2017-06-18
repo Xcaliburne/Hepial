c7ec0d3e7737dcb746046d5ff67faaf5d7e9ab08
Vince <vincetournier@msn.com>
2017-06-18 11:58:52 +0200

    can now work with constant writing

--- ch/hepia/Compilateur/Arbre/Ecrire.java
+++ ch/hepia/Compilateur/Arbre/Ecrire.java
@@ -2,9 +2,19 @@ package ch.hepia.Compilateur.Arbre;
 
 public class Ecrire extends Instruction{
 	private Expression texte;
+	private String constante;
+	private boolean isString = false;
+
 	public Ecrire(Expression t, int lig) {
         super(lig);
         this.texte = t;
+		isString = false;
+    }
+
+	public Ecrire(String str, int lig) {
+        super(lig);
+        this.constante = str;
+		isString = true;
     }
 
     public Expression getTexte() {
@@ -14,7 +24,20 @@ public class Ecrire extends Instruction{
     public void setTexte(Expression t) {
         this.texte = t;
     }
-    
+
+	public String getString() {
+        return this.constante;
+    }
+
+	public void setString(String str) {
+        this.constante = str;
+    }
+
+	public boolean isString()
+	{
+		return isString;
+	}
+
     public Object accepter(Visiteur v) {
         return v.visiter(this);
     }
